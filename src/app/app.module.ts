5fbe0cb180e400c23f1ba847a336abb7390bb4cd
mdelta <dev@vantero.de>
2023-06-27 22:06:10 +0200

    Add Material Design and icons
    Without Google API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,23 @@ import { AppComponent } from './app.component';
 import { EducationComponent } from './education/education.component';
 import { EducationListComponent } from './education/education-list/education-list.component';
 import { EducationItemComponent } from './education/education-item/education-item.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [AppComponent, EducationListComponent, EducationItemComponent, EducationComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    EducationListComponent,
+    EducationItemComponent,
+    EducationComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })

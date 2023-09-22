484d39af86366a6aada988e36c9f74f295998caf
Andreas Pieger <andreas.pieger@epg.com>
2023-09-22 11:09:33 +0200

    Set page title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import * as data from '../assets/resume.json';
+import { Title } from '@angular/platform-browser';
 import { Education } from 'src/model/education.model';
 import { SocialMedia } from 'src/model/socialmedia.model';
+import * as data from '../assets/resume.json';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,11 @@ export class AppComponent implements OnInit {
   socialProfiles: SocialMedia[] = [];
   educationData: Education[] = [];
 
+  constructor(private readonly titleService: Title) {}
+
   ngOnInit() {
+    this.titleService.setTitle(`${this.resumeData.basics.name}, Lebenslauf`);
+
     for (const item of this.resumeData.basics.profiles) {
       const social = new SocialMedia(item);
       this.socialProfiles.push(social);

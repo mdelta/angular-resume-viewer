47eb07eed3cfa151da3278be0fc05fa8068e8594
Andreas Pieger <andreas.pieger@epg.com>
2023-09-22 13:41:52 +0200

    Arrange import statements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlipcardComponent } from './flipcard/flipcard.component';
 
-import { EducationComponent } from './education/education.component';
-import { EducationListComponent } from './education/education-list/education-list.component';
-import { EducationItemComponent } from './education/education-item/education-item.component';
-
+import { NameComponent } from './name/name.component';
+import { PersonalComponent } from './personal/personal.component';
 import { SocialComponent } from './social/social.component';
 import { SocialListComponent } from './social/social-list/social-list.component';
 import { SocialItemComponent } from './social/social-item/social-item.component';
+import { EducationComponent } from './education/education.component';
+import { EducationListComponent } from './education/education-list/education-list.component';
+import { EducationItemComponent } from './education/education-item/education-item.component';
+import { WorkComponent } from './work/work.component';
+import { WorkItemComponent } from './work/work-item/work-item.component';
+import { WorkListComponent } from './work/work-list/work-list.component';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatChipsModule } from '@angular/material/chips';
@@ -20,14 +25,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 
-import { NameComponent } from './name/name.component';
 import { VcardComponent } from './vcard/vcard.component';
-import { FlipcardComponent } from './flipcard/flipcard.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { PersonalComponent } from './personal/personal.component';
-import { WorkComponent } from './work/work.component';
-import { WorkItemComponent } from './work/work-item/work-item.component';
-import { WorkListComponent } from './work/work-list/work-list.component';
 
 @NgModule({
   declarations: [

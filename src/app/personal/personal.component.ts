96227fd16b82897e2433f277fc8239e06a89d880
Andreas Pieger <andreas.pieger@epg.com>
2023-09-22 15:57:30 +0200

    Add personal data

--- src/app/personal/personal.component.ts
+++ src/app/personal/personal.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Basics } from 'src/model/basics.model';
+import { Location } from 'src/model/location.model';
 
 @Component({
   selector: 'app-personal',
   templateUrl: './personal.component.html',
-  styleUrls: ['./personal.component.scss']
+  styleUrls: ['./personal.component.scss'],
 })
 export class PersonalComponent {
-
+  @Input() item!: Basics;
+  @Input() location!: Location;
 }

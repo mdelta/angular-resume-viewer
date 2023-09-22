3b315a08face8d1f3c43a773563feef9e0de7347
Andreas Pieger <andreas.pieger@epg.com>
2023-09-22 13:12:17 +0200

    Add location model

---
+++ src/model/location.model.ts
@@ -0,0 +1,19 @@
+export class Location {
+  address = '';
+  postalCode = '';
+  city = '';
+  countryCode = '';
+  region = '';
+
+  constructor(item: unknown) {
+    if (!item) throw new Error('No value given');
+    if (typeof item !== 'object') throw new Error('No object given');
+
+    if ('address' in item && typeof item.address === 'string') this.address = item.address;
+    if ('postalCode' in item && typeof item.postalCode === 'string') this.postalCode = item.postalCode;
+    if ('city' in item && typeof item.city === 'string') this.city = item.city;
+    if ('countryCode' in item && typeof item.countryCode === 'string')
+      this.countryCode = item.countryCode;
+    if ('region' in item && typeof item.region === 'string') this.region = item.region;
+  }
+}

761cf27ccdc8fb68ae32f26bacd955f9b13deaf4
Andreas Pieger <andreas.pieger@epg.com>
2023-11-06 16:31:04 +0100

    Add background image with blur
    Experimenting with multiple images, not decided yet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ResumeModule } from './resume/resume.module';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    ResumeModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, ResumeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
